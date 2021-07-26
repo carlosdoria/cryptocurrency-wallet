a68359f66471ac6801ceb293c754fb34fb905887
carlosdoria <carlosdoria953@gmail.com>
2021-07-26 11:53:21 -0300

    feat: create validation on dashboard page route

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,15 +1,27 @@
 
 import Divider from '@material-ui/core/Divider'
 import Grid from '@material-ui/core/Grid'
+import { useRouter } from 'next/router'
 
 import { Layout, UserCurrencyCard } from 'components'
+import { useEffect } from 'react'
+import { auth } from 'services/firebase'
 
 import * as S from './styles'
 
 export default function Dashboard () {
+  const router = useRouter()
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (!user) router.push('/')
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
   return (
-
     <Layout>
       <Grid container spacing={3}>
         {/* Recent Deposits */}
